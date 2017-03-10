1d0d5970fc7f17a7042b7ad112fe590440b5f7e8
Jenessa <jenessaw31@gmail.com>
2017-03-09 22:36:25 -0600

    added gif

--- index.android.js
+++ index.android.js
@@ -1,53 +1,113 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Alert, AppRegistry, Dimensions, Image, Text, ScrollView, View } from 'react-native';
+import { Icon } from 'react-native-elements';
+import Tabs from 'react-native-tabs';
+import { ActionConst, Scene, Router} from 'react-native-router-flux';
+
+import colors from './colors';
+import styles from './styles';
+
+import Home from './Home';
+import NewProject from './NewProject';
+import Profile from './Profile';
+import SingleProject from './SingleProject';
+import Budget from './Budget';
+import ChangeBudgetModal from './ChangeBudgetModal';
+import Transactions from './Transactions';
+import NewTransaction from './NewTransaction';
+import Tasks from './Tasks';
+import NewTask from './NewTask';
+import TaskModal from './TaskModal';
+import Photos from './Photos';
+import NewPhoto from './NewPhoto';
+import Materials from './Materials';
+import NewMaterial from './NewMaterial';
+import MaterialModal from './MaterialModal';
+
+import Example from './Example';
+
+
+var width = Dimensions.get('window').width; //full width
+var height = Dimensions.get('window').height; //full height
 
 export default class diyApp extends Component {
+
   render() {
+    // Simple component to render something in place of icon
+    const HomeTabIcon = ({ selected, title }) => {
+      return (
+        <View>
+          {/* <Icon
+            name='home'
+            size={25}
+            iconStyle={{color: selected ? '#FFC107' :'#212121', fontWeight: selected ? 'bold' :'normal'}}/>
+          <Text style={{color: selected ? 'white' :'black', fontWeight: selected ? 'bold' :'normal'}}>{title}</Text> */}
+        </View>
+      );
+    }
+    const NewProjectTabIcon = ({ selected, title }) => {
+      return (
+        <View>
+          {/* <Icon
+            name='add'
+            size={25}
+            iconStyle={{color: selected ? '#FFC107' :'#212121', fontWeight: selected ? 'bold' :'normal'}}/>
+          <Text style={{color: selected ? 'white' :'black', fontWeight: selected ? 'bold' :'normal'}}>{title}</Text> */}
+        </View>
+      );
+    }
+    const ProfileTabIcon = ({ selected, title }) => {
+      return (
+        <View>
+          {/* <Icon
+            name='person'
+            size={25}
+            iconStyle={{color: selected ? '#FFC107' :'#212121', fontWeight: selected ? 'bold' :'normal'}}/>
+          <Text style={{color: selected ? 'white' :'black', fontWeight: selected ? 'bold' :'normal'}}>{title}</Text> */}
+        </View>
+      );
+    }
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Router>
+        <Scene key="root">
+          {/* <Scene key="example" component={Example} hideNavBar/> */}
+
+          {/* Tab Container */}
+          <Scene key="tabbar" tabs={true} tabBarStyle={{height: 0,  backgroundColor: 'transparent'}} type={ActionConst.REPLACE} >
+            {/* Tab and it's scenes */}
+            {/* <Scene key="camera" component={CameraApp} /> */}
+            <Scene key="homeTab" title="Home" icon={HomeTabIcon} hideNavBar>
+              <Scene key="home" component={Home} title="Home"/>
+            </Scene>
+            {/* Tab and it's scenes */}
+            <Scene key="newProjectTab" title="New Project" icon={NewProjectTabIcon} hideNavBar>
+              <Scene key="newProject" component={NewProject} title="New Project"/>
+            </Scene>
+            {/* Tab and it's scenes */}
+            <Scene key="profileTab" hideNavBar title="Profile" icon={ProfileTabIcon}>
+              <Scene key="profile" component={Profile} title="Profile" />
+            </Scene>
+          </Scene>
+          <Scene hideNavBar key="singleProjectHold">
+            <Scene key="singleProject" component={SingleProject} />
+            <Scene key="materials" component={Materials} />
+            <Scene key="newMaterialModal" component={NewMaterial} />
+            <Scene key="materialModal" component={MaterialModal} />
+            <Scene key="budget" component={Budget} />
+            <Scene key="updateBudget" component={ChangeBudgetModal} />
+            <Scene key="newTransactionModal" component={NewTransaction} />
+            <Scene key="photos" component={Photos} />
+            <Scene direction="vertical" key="newPhotoModal" component={NewPhoto} />
+            <Scene key="tasks" component={Tasks} />
+            <Scene direction="vertical" key="newTaskModal" component={NewTask}/>
+            <Scene key="taskModal" component={TaskModal} />
+          </Scene>
+        </Scene>
+      </Router>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
 
 AppRegistry.registerComponent('diyApp', () => diyApp);
