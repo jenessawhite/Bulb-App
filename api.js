327227a2ad02e25ae3411c9bf8d1d7ae61fc1f7a
Jim Cummins <jimthedev@gmail.com>
2017-03-17 22:37:26 -0500

    fix local api in dev on android

--- api.js
+++ api.js
@@ -1,7 +1,12 @@
+import { Platform } from 'react-native'
+
 export default function api() {
   if (process.env.NODE_ENV === "production") {
     return 'http://localhost:3001/api';
   } else {
-   return 'http://localhost:3001/api';
+    if(Platform.OS === 'android') {
+      return 'http://10.0.2.2:3001/api'
+    }
+    return 'http://localhost:3001/api';
   }
 }
